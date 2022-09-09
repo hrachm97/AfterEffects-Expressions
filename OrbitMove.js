d8c541d2ad83cb83fe5e1ff83b3ff42b0948cbd1
Hrach Mkrtumyan <102722152+hrachm97@users.noreply.github.com>
2022-09-09 19:38:10 +0400

    Update OrbitMove.js

--- OrbitMove.js
+++ OrbitMove.js
@@ -1,13 +1,10 @@
-
-
-
-let angle = degreesToRadians(effect("Angle")("Angle")-90);
+let angle = degreesToRadians(effect("Angle")("Angle") - 90);
 let x = Math.cos(angle);
 let y = Math.sin(angle);
 let r1 = effect("r1")("Slider");
 let r2 = effect("r2")("Slider");
 try {
-effect("Layer center")("Layer").transform.position+[r1*x,r2*y]
+	effect("Layer center")("Layer").transform.position+[r1*x,r2*y]
 } catch(err) {
-	position+[r1*x,r2*y]
+	position + [r1 * x, r2 * y]
 }
