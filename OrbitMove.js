096d6096183887f315ccafe0074e0cb8e963fc60
Hrach Mkrtumyan <102722152+hrachm97@users.noreply.github.com>
2022-09-09 19:38:44 +0400

    Update OrbitMove.js

--- OrbitMove.js
+++ OrbitMove.js
@@ -4,7 +4,7 @@ let y = Math.sin(angle);
 let r1 = effect("r1")("Slider");
 let r2 = effect("r2")("Slider");
 try {
-	effect("Layer center")("Layer").transform.position+[r1*x,r2*y]
+	effect("Layer center")("Layer").transform.position + [r1 * x, r2 * y];
 } catch(err) {
-	position + [r1 * x, r2 * y]
+	position + [r1 * x, r2 * y];
 }
