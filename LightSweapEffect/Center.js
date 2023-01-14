26a00142891896464771437e967825c4b6770b19
Hrach Mkrtumyan <102722152+hrachm97@users.noreply.github.com>
2023-01-14 15:44:14 +0400

    Create Center.js

---
+++ LightSweapEffect/Center.js
@@ -0,0 +1,16 @@
+const _duration = effect("duration")("Slider");
+const Speed = 1 / _duration; // sweap per second
+const sweaper = time % _duration; // loops from 0 to duration amount in real time speed
+const unitSweap = sweaper*Speed; // loops from 0 to 1 in duration time
+const cosinus_sweap = Math.cos(Math.PI * unitSweap); // loops from 1 to -1
+const slope = effect("CC Light Sweep")("Direction");
+const rect = thisLayer.sourceRectAtTime();
+const has_size = !(width === thisComp.width && height === thisComp.height);
+const Height = has_size ? height : rect.height*scale[1]/100;
+const Width = has_size ? width : rect.width*scale[0]/100;
+const slope_interval = (slope > 0 ? unitSweap : 1 - unitSweap) * Math.tan(degreesToRadians(slope)) * Height;
+const width_interval = cosinus_sweap * effect("CC Light Sweep")("Width") * 2 / Math.cos(degreesToRadians(slope));
+const top_left = position - [anchorPoint[0]*scale[0],anchorPoint[1]*scale[1]]/100 + [rect.left*scale[0]/100, rect.top*scale[1]/100];
+let [x,y] = has_size ? [0,0] : [top_left[0], top_left[1]];
+
+[x + unitSweap * Width + slope_interval - width_interval, y]
