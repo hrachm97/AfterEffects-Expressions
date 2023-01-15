d238d7b50e19c68ee5ff45f0d8ac407c7798eb18
Hrach Mkrtumyan <102722152+hrachm97@users.noreply.github.com>
2023-01-15 15:32:06 +0400

    Update Direction.js

--- LightSweapEffect/Direction.js
+++ LightSweapEffect/Direction.js
@@ -1,2 +1,8 @@
 let slope = effect("Angle Control")("Angle");
-radiansToDegrees(Math.atan(Math.tan(degreesToRadians(slope))))
+radiansToDegrees(
+	Math.atan(
+		Math.tan(
+			degreesToRadians(slope)
+			)
+		)
+	)
