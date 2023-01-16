219c5a50c84f92691c3b48954220699278c50123
Hrach Mkrtumyan <102722152+hrachm97@users.noreply.github.com>
2023-01-16 14:17:08 +0400

    Update position.js

--- OrbitMove/position.js
+++ OrbitMove/position.js
@@ -2,10 +2,9 @@ let angle = degreesToRadians(effect("Angle")("Angle") - 90);
 let x = Math.cos(angle);
 let y = Math.sin(angle);
 let r1 = effect("r1")("Slider");
-let r2 = effect("r2")("Slider");
-
+let r2 = effect("elipse")("Checkbox") == true ? effect("r2")("Slider") : effect("r1")("Slider")
 try {
-	result = effect("Layer center")("Layer").transform.position + [r1 * x, r2 * y];
+	effect("Layer center")("Layer").position+[r1 * x, r2 * y]
 } catch(err) {
-	result = position + [r1 * x, r2 * y];
+	position + [r1 * x, r2 * y]
 }
